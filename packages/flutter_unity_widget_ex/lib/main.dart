31b5ee9139631fb533d01e98c3c544978c4c2c6e
htsuruo <tsuru.dev@gmail.com>
2024-07-17 23:23:14 +0900

    適当にUnityViewを配置

--- packages/flutter_unity_widget_ex/lib/main.dart
+++ packages/flutter_unity_widget_ex/lib/main.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
+import 'package:flutter_unity_widget/flutter_unity_widget.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +12,46 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return const MaterialApp(home: _UnityView());
+  }
+}
+
+class _UnityView extends StatefulWidget {
+  const _UnityView();
+
+  @override
+  State<_UnityView> createState() => _UnityViewState();
+}
+
+class _UnityViewState extends State<_UnityView> {
+  late UnityWidgetController _controller;
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Unity on Flutter'),
+      ),
+      body: Center(
+        child: UnityWidget(
+          onUnityCreated: (controller) {
+            _controller = controller;
+          },
+          onUnitySceneLoaded: (scene) {
+            if (scene != null) {
+              print('Received scene loaded from unity: ${scene.name}');
+              print(
+                  'Received scene loaded from unity buildIndex: ${scene.buildIndex}');
+            } else {
+              print('Received scene loaded from unity: null');
+            }
+          },
         ),
       ),
     );
